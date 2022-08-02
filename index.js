392296bc21a4bc78932b511e055d0b4d237fda08
Karol Dobrek <Inakus@wp.pl>
2022-08-02 19:05:52 +0200

    changed to only data

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const passport = require('passport');
 const mongoose = require('mongoose');
-const passportLocalMongoose = require('passport-local-mongoose');
-const session =  require('express-session');
 require('dotenv').config();
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended : false}));
-app.use(session({
-    secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {secure: true}
-}));
-app.use(passport.initialize());
-app.use(passport.session());
 
 const mongoDBLink = process.env.MONGO_DB_LINK
 
@@ -26,64 +15,57 @@ async function mongoConnect() {
 
 mongoConnect().catch(err => {console.error(err)});
 
-const userSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    blog: [{title: String, content: String}]
+const blogSchema = new mongoose.Schema({
+    title: String,
+    content: String
 });
 
-userSchema.plugin(passportLocalMongoose);
-
-const User = new mongoose.model('User', userSchema);
-
-passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+const Blog = new mongoose.model('Blog', blogSchema);
 
 mongoConnect(mongoDBLink).catch(err => {console.log(err)});
 
 app.route('/')
     .get((req, res) => {
-        User.find((err, users) => {
+        Blog.find((err, users) => {
             err ? console.log(err) : res.send(users);
         });
     })
     .post((req, res) => {
-        User.register({username: req.body.username}, req.body.password, (err, user) => {
-            err ? console.log(err) : passport.authenticate('local')(req, res, () => {
-                res.redirect('/user/' + req.body.username);
-            })
+        const newPost = new Blog({
+            title: req.body.title,
+            content: req.body.content
         });
+        newPost.save(err => {err ? console.log(err) : res.send('Succesfully added new Post')});
     })
     .delete((req, res) => {
-        User.deleteMany(err => {
+        Blog.deleteMany(err => {
             err ? console.log(err) : res.send('Succesfully deleted all account');
         })
     });
 
-app.route('/user/:userName')
+app.route('/:title')
     .get((req, res) => {
-        const userName = req.params.userName;
-        User.find({username: userName}, (err, foundUser) => {
-            foundUser ? res.send(foundUser) : console.log(err);
+        const title = req.params.title;
+        Blog.find({title: title}, (err, foundPost) => {
+            foundPost ? res.send(foundPost) : console.log(err);
         });
     })
     .put((req, res) => {
-        const userName = req.params.userName;
-        User.replaceOne({username: userName}, {blog: [{title: req.body.title, content: req.body.content}]}, err => {
-            err ? res.send(err) : res.send("Succesfully update article.");
+        const title = req.params.title;
+        Blog.replaceOne({title: title}, {title: req.body.title, content: req.body.content}, err => {
+            err ? res.send(err) : res.send("Succesfully update post.");
         });
     })
     .patch((req, res) => {
-        const userName = req.params.userName;
-        User.updateOne({username: userName}, {blog: [{title: req.body.title, content: req.body.content}]}, err => {
-            err ? res.send(err) : res.send("Succesfully update article.");
+        const title = req.params.title;
+        Blog.updateOne({title: title}, {title: req.body.title, content: req.body.content}, err => {
+            err ? res.send(err) : res.send("Succesfully update post.");
         }); 
     })
     .delete((req, res) => {
-        const userName = req.params.userName;
-        User.deleteOne({username: userName}, err => {
-            err ? res.send(err) : res.send("Succesfully deleted user");
+        const title = req.params.title;
+        Blog.deleteOne({title: title}, err => {
+            err ? res.send(err) : res.send("Succesfully deleted post");
         });
     });
 
