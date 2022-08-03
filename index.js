7638ed60d28f20869023151a50466a159a64b7f8
Karol Dobrek <Inakus@wp.pl>
2022-08-03 18:27:40 +0200

    added cors to index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended : false}));
+app.use(cors());
 
 const mongoDBLink = process.env.MONGO_DB_LINK
 
